4b886128589914e56480f43c592146557fa1610e
gns1643 <gns1643@naver.com>
2025-06-22 17:53:32 +0900

    공과 카메라 움직이기

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float rotationSpeed;
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+}
